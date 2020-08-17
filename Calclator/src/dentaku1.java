f9b794acc5b3f1ee2bae856b1327d19aa01abad0
tokoron <[a.tokoro5@gmail.com]>
2020-08-17 11:27:44 +0900

    Add other operations

--- Calclator/src/dentaku1.java
+++ Calclator/src/dentaku1.java
@@ -6,16 +6,25 @@ public class dentaku1 {
 
 		Scanner scanner = new Scanner(System.in);
 		String inputval = scanner.nextLine();
-
 		String[] inputList = inputval.split(" ");
 
 		int input1 = Integer.parseInt(inputList[0]);
 		int input2 = Integer.parseInt(inputList[2]);
 		String ope = inputList[1];
 
-		int sum = input1 + input2;
-
-		System.out.println(input1 + " " + ope + " " + input2 + " = " + sum);
+		if(ope.equals("+")) {
+			int add = input1 + input2;
+			System.out.println(add);
+		}else if(ope.equals("-")) {
+			int sub = input1 - input2;
+			System.out.println(sub);
+		}else if(ope.equals("*")) {
+			int mult = input1 * input2;
+			System.out.println(mult);
+		}else if(ope.equals("/")) {
+			int divi = input1 / input2;
+			System.out.println(divi);
+		}
 
 		scanner.close();
 
